6bf83c0a202008ea2495ce17d9b6984cb644efb3
Subham Jain <51121457+jainofcsubham@users.noreply.github.com>
2023-01-27 10:27:21 +0530

    Added Message Queue (#2)
    * feat: Layouting
    
    * feat: Message Queue Implementation
    
    * feat: Minor modifications in the flow
    
    * feat: Added Size and List topic
    
    * feat: Structural Changes
    
    * feat: Integrated APIs with Data Structure

---
+++ src/controller/main.py
@@ -0,0 +1,243 @@
+from src.controller.utils import raise_error, raise_success
+
+
+class Message_Queue:
+    """
+    The class with functions to perform actions on the Message Queue.
+
+    Private variables:-
+
+    1. __topics
+
+    A dict which stores all the topics published in the Message Queue.
+
+    Example format\n
+    __topics = {
+        'A' : { #'A' is the topic name.
+            'producers' : [], #List of producersId which can publish to the topic.
+            'consumers' : [], #List of consumersId which are subscribed to the topic.
+            'messages'  : [ #List of messages which are yet to be consumed by the consumers.
+                {
+                    'message'     : 'Some message', #The message
+                    'subscribers' : 10 #The number of consumers that can consume this message.
+                }
+            ],
+            'bias': 10,#The number of messages that are consumed by all the subscribers
+        }
+    }
+
+    2. __consumers
+
+    A dict which stores all the consumers that are registered in the system.
+
+    Example format\n
+    __consumers = {
+        '5' : { #The key is the unique id of the consumer.
+            'topics' : {
+                'A' : { #'A' is the topic to which consumer has subscribed to.
+                    'position' : 2 #This tells from which position the consumer should consume message from.
+                }
+            }
+        }
+    }
+
+    2. __producers
+
+    A dict which stores all the producers that are registered in the system.
+
+    Example format\n
+    __producers = {
+        '1' : { #The key is the unique id of the producer.
+            'topic' : 'A' #'A' is the topic to which the producer is subscribed to.
+        }
+    }
+    """
+
+    def __init__(self):
+        """
+        Constructor of the class.
+        """
+        self.__topics = {}
+        self.__consumers = {}
+        self.__producers = {}
+
+    def init(self):
+        """
+        This is used to initialize the class members from the persistent DB.
+        """
+        return 0
+
+    def add_topic(self, topic_name: str):
+        """
+        Pushes a topic to the __topics member of the class.\n
+        Anyone can add a topic to the system.\n
+        Error handling:-
+
+        1) If the topic already exists then throw an error.
+        """
+        # Error handling
+        if topic_name in self.__topics:
+            return raise_error("Topic " + topic_name + " already exists.")
+
+        # Adding topic to the topics dict with no producers, no consumers and no messages.
+        self.__topics[topic_name] = {
+            "producers": [],
+            "consumers": [],
+            "messages": [],
+            "bias" : 0
+        }
+        return raise_success("Topic " + topic_name + " added successfully.")
+
+    # To push a message to the topic queue
+    def publish_message(self, producer_id: int, topic_name: str, message: str):
+        """
+        Publishes a message to the provided topic.\n
+        Only a producer who's registered with the topic can publish messages in it.\n
+        Error handling:-
+
+        1) If the topic doesn't exist throw an error
+        2) If producer with producer_id doesn't exist then throw an error
+        3) If producer hasn't registered to any topic or producer has nto registered to this topic then throw an error
+        """
+        # Error handling
+        if topic_name not in self.__topics:
+            return raise_error("Topic " + topic_name + " doesn't exist.")
+        if producer_id not in self.__producers:
+            return raise_error("Producer doesn't exist.")
+        if "topic" not in self.__producers[producer_id] or self.__producers[producer_id]["topic"] != topic_name:
+            return raise_error("Producer cannot publish to " + topic_name + ".")
+        if len(self.__topics[topic_name]["consumers"]) == 0:
+            return raise_error("No subscribers so cannot publish to the topic.")
+
+        # What if there are no subscribers? Shall we even add the message to the topic?
+        # Adding the message to the topic queue if there are subscribers otherwise not.
+        if len(self.__topics[topic_name]["consumers"]) > 0:
+            self.__topics[topic_name]["messages"].append({
+                "message": message,
+                "subscribers": len(self.__topics[topic_name]["consumers"])
+            })
+        return raise_success("Message " + message + " added successfully to " + topic_name + ".")
+
+    # To register a producer
+    def register_producer(self, topic_name: str):
+        """
+        Creates a producer in the system
+        """
+        # Generating unique Id for a producer
+        producer_id = len(self.__producers) + 1
+        # Adding producer to the producers dict
+        self.__producers[producer_id] = {}
+        if topic_name not in self.__topics:
+            self.add_topic(topic_name)
+        self.add_producer_to_topic(topic_name, producer_id)
+        return raise_success("Producer registered successfully.", {"producer_id": producer_id})
+
+    # To register a consumer
+    def register_consumer(self, topic_name : str):
+        """
+        Creates a register in the system
+        """
+        # Generating unique Id for a consumer
+        if topic_name not in self.__topics:
+                return raise_success("Topic doesn't exist.")
+        consumer_id = len(self.__consumers) + 1
+        # Adding consumer to the producers dict
+        self.__consumers[consumer_id] = {"topics": {}}
+        self.subscribe_to_topic(topic_name,consumer_id)
+        return raise_success("Consumer registered successfully.", {"consumer_id": consumer_id})
+
+    # For consumer to subscribe to a topic
+    def subscribe_to_topic(self, topic_name, consumer_id):
+        """
+        This adds consumers under a topic.\n
+        Error handling:-
+
+        1) If the topic doesn't exist then throw an error
+        2) If the consumer with consumer_id doesn't exist then throw an error
+        3) If the consumer is already subscribed to the topic then throw an error
+        """
+        if topic_name not in self.__topics:
+            return raise_error("Topic " + topic_name + " doesn't exist.")
+        if consumer_id not in self.__consumers:
+            return raise_error("Consumer doesn't exist.")
+        if topic_name in self.__consumers[consumer_id]["topics"]:
+            return raise_error("Consumer is already subscribed to the topic " + topic_name + ".")
+
+        self.__consumers[consumer_id]["topics"][topic_name] = {
+            "position": self.__topics[topic_name]["bias"] + len(self.__topics[topic_name]["messages"])
+        }
+        self.__topics[topic_name]["consumers"].append(consumer_id)
+        return raise_success("Consumer is now subscribed to topic " + topic_name + ".")
+
+    # For producer to register as a publisher for a topic
+    def add_producer_to_topic(self, topic_name, producer_id):
+        """
+        This will add producer as a publisher for the given topic.\n
+        Error handling:-
+
+        1) If the topic doesn't exist then throw an error
+        2) If the producer with producer_id doesn't exist then throw an error
+        3) If the producer is already a publisher in another topic then throw an error
+        4) If the producer is already a publisher in same topic then throw an error
+        """
+        if topic_name not in self.__topics:
+            return raise_error("Topic " + topic_name + " doesn't exist.")
+        if producer_id not in self.__producers:
+            return raise_error("Invalid producer.")
+        if "topic" in self.__producers[producer_id] and self.__producers[producer_id]["topic"] != topic_name:
+            return raise_error("Producer cannot register to more than one topic.")
+        if "topic" in self.__producers[producer_id] and self.__producers[producer_id]["topic"] == topic_name:
+            return raise_error("Producer is already registered under topic " + topic_name + ".")
+        self.__producers[producer_id]["topic"] = topic_name
+        self.__topics[topic_name]["producers"].append(producer_id)
+        return raise_success("Producer can now publish messages in topic " + topic_name + ".")
+
+    # For consuming a message from the Message Queue
+
+    def consume_message(self, topic_name, consumer_id):
+        """
+        This will send the message at the start of the queue to be consumed by the consumer.\n
+        Error handling:-
+
+        1) If the topic doesn't exist then throw an error
+        2) If the consumer with consumer_id doesn't exist then throw an error
+        3) If the consumer is not a subscriber of that topic then throw an error
+        """
+        if topic_name not in self.__topics:
+            return raise_error("Topic " + topic_name + " doesn't exist.")
+        if consumer_id not in self.__consumers:
+            return raise_error("Consumer doesn't exist.")
+        if topic_name not in self.__consumers[consumer_id]["topics"]:
+            return raise_error("Consumer is not subscribed to " + topic_name + ".")
+        if len(self.__topics[topic_name]["messages"]) <= 0 or self.__consumers[consumer_id]["topics"][topic_name]["position"] - self.__topics[topic_name]["bias"] >= len(self.__topics[topic_name]["messages"]):
+            return raise_error("No new message is published to " + topic_name + ".")
+        message_position = self.__consumers[consumer_id]["topics"][topic_name]["position"] - self.__topics[topic_name]["bias"]
+        self.__topics[topic_name]["messages"][message_position]["subscribers"] = self.__topics[
+            topic_name]["messages"][message_position]["subscribers"] - 1
+        message_to_send = self.__topics[topic_name]["messages"][message_position]
+        self.__consumers[consumer_id]["topics"][topic_name]["position"] = self.__consumers[consumer_id]["topics"][topic_name]["position"] + 1
+        if message_to_send["subscribers"] == 0:
+            self.__topics[topic_name]["bias"] = self.__topics[topic_name]["bias"] + 1
+            self.__topics[topic_name]["messages"].pop(0)
+            # for consumer in self.__topics[topic_name]["consumers"]:
+            #     self.__consumers[consumer]["topics"][topic_name]["position"] = self.__consumers[consumer]["topics"][topic_name]["position"] - 1
+        return raise_success("Message fetched successfully.", {
+            "message": message_to_send["message"]
+        })
+
+    def list_topics(self):
+        all_topics = list(self.__topics.keys())
+        return raise_success("Successfully fetched topics.", {"topics": all_topics})
+
+    def log_size(self, topic_name: str, consumer_id : int):
+        if topic_name not in self.__topics:
+            return raise_error("Topic " + topic_name + " doesn't exist.")
+        if consumer_id not in self.__consumers:
+            return raise_error("Consumer doesn't exist.")
+        if topic_name not in self.__consumers[consumer_id]["topics"]:
+            return raise_error("Consumer is not subscribed to " + topic_name + ".")
+        print(self.__topics)
+        print(self.__consumers)
+        if len(self.__topics[topic_name]["messages"]) <= 0 or self.__consumers[consumer_id]["topics"][topic_name]["position"] - self.__topics[topic_name]["bias"] >= len(self.__topics[topic_name]["messages"]):
+            return raise_success("Successfully fetched size for topic " + topic_name + ".", {"size": 0 })
+        return raise_success("Successfully fetched size for topic " + topic_name + ".", {"size": len(self.__topics[topic_name]["messages"]) - self.__consumers[consumer_id]["topics"][topic_name]["position"] + self.__topics[topic_name]["bias"] })
